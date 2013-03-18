7831d0841f124da372e00ba445bfa374f1939a4b
sorra <dawnstar@163.com>
2013-03-18 23:23:33 +0800

    richer testing

--- grids/test/grids/SceneTest.java
+++ grids/test/grids/SceneTest.java
@@ -21,6 +21,16 @@ public class SceneTest {
 	StreamService streamService;
 	GridService gridService;
 	
+	long root = 1;
+	long society;
+	long culture;
+	long economy;
+	long tech;
+	
+	long view;
+	long art;
+	long painting;
+	long music;
 	
 	@Before
 	public void setUp() {
@@ -32,8 +42,6 @@ public class SceneTest {
 		tagService = ac.getBean(TagService.class);
 		streamService = ac.getBean(StreamService.class);
 		gridService = ac.getBean(GridService.class);
-		
-//		tagService.init();
 	}
 	
 	@Test
@@ -46,9 +54,15 @@ public class SceneTest {
 	}
 	
 	private void tag() {
-		tagService.newTag("社会", 1);
-		tagService.newTag("文化", 1);
-		tagService.newTag("科技", 1);
+		society = tagService.newTag("社会", root);
+		culture = tagService.newTag("文化", root);
+		economy = tagService.newTag("经济", root);
+		tech = tagService.newTag("科技", root);
+		
+		view = tagService.newTag("观察", society);
+		art = tagService.newTag("艺术", culture);
+		painting = tagService.newTag("绘画", art);
+		music = tagService.newTag("音乐", art);
 	}
 	
 	private void user() {
@@ -58,12 +72,14 @@ public class SceneTest {
 	}
 	
 	private void relation() {
-		relationService.follow(1, 2, new long[]{2, 3});
-		relationService.follow(1, 3, new long[]{2, 3});
-		relationService.follow(2, 1, new long[]{2});
-		relationService.follow(3, 1, new long[]{2});
-		relationService.follow(2, 3, new long[]{2});
-		relationService.follow(3, 2, new long[]{2});
+		relationService.follow(1, 2, new long[]{society, culture});
+		relationService.follow(1, 3, new long[]{society, culture});
+		
+		relationService.follow(2, 1, new long[]{music, view});
+		relationService.follow(2, 3, new long[]{music});
+		
+		relationService.follow(3, 1, new long[]{tech, view});
+		relationService.follow(3, 2, new long[]{art});
 	}
 	
 	private void post() {
@@ -76,10 +92,10 @@ public class SceneTest {
 		blogService.blog(3, "Gamma",
 				"GVIWSWOWdv*&[\n(@)\tpsp\nT^HJ",
 				new long[]{1, 2});
-		tweetService.tweet(1, "Post at root.", new long[]{1});
-		tweetService.tweet(1, "HUUSF", new long[]{2, 3});
-		tweetService.tweet(2, "Better *Wahaha!", new long[]{2});
-		tweetService.tweet(3, "Seen less gebdpro", new long[]{3});
+		tweetService.tweet(1, "Post at root.", new long[]{root});
+		tweetService.tweet(1, "HUUSF", new long[]{view});
+		tweetService.tweet(2, "Better *Wahaha!", new long[]{music});
+		tweetService.tweet(3, "Seen less gebdpro", new long[]{tech});
 	}
 	
 	private void istream() {
