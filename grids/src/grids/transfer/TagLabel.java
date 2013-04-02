ad1a9c61c55680629fc4546d5371e8be8e8461db
sorra <dawnstar@163.com>
2013-04-02 15:35:26 +0800

    tag label

---
+++ grids/src/grids/transfer/TagLabel.java
@@ -0,0 +1,19 @@
+package grids.transfer;
+
+import grids.entity.Tag;
+
+public class TagLabel {
+	private long id;
+	private String name;
+	public TagLabel(Tag tag) {
+		id = tag.getId();
+		name = tag.getName();
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+}
