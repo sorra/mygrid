70f2cdeabd55403bb93c86551f91b374cf5104a0
sorra <dawnstar@163.com>
2013-04-02 22:38:33 +0800

    tweet shows date and tags

--- grids/WebContent/js/home.js
+++ grids/WebContent/js/home.js
@@ -5,30 +5,37 @@ function renderIstream(stream) {
 	var streamHtml = '';
 	dojo.forEach(stream.items, function(item){
 		var itemHtml = '<div class="item row">';
+		
 		itemHtml += '<div class="avatar span1">';
 		itemHtml += ('<a href="#' + item.authorId + '">');
-		itemHtml += ('<img src="' + item.avatar + '"/></a>');
-		itemHtml += '</div>';
+		itemHtml += ('<img src="' + '/grids/rs/img/wang.jpg'/*item.avatar*/ + '"/></a></div>');
+		
 		itemHtml += '<div class="tweet span7">';
-		itemHtml += ('<a class="author" href="#' + item.authorId +
+		itemHtml += ('<div class="row"><a class="author" href="#' + item.authorId +
 				'"><b>' + item.authorName + '</b></a>');
-		itemHtml += wrapInTag(item.content, 'p');
-		itemHtml += '</div></div>';
+		itemHtml += (wrapInTag(item.content, 'p') + '</div>');
+		itemHtml += ('<div class="row"><a>' + new Date(item.date).toLocaleString()
+				+ ' </a><a>' + showTags(item.tags) + '</a></div>');
+		itemHtml += '</div>';
+		
+		itemHtml += '</div>';
 		streamHtml += itemHtml;
 	});
 	streamDiv.innerHTML = streamHtml;
-//<div class="item row">
-//	<div class="avatar span1"><a href="#"><img src="rs/img/dingcl.jpg"/></a></div>
-//	<div class="tweet span7">
-//		<a class="author" href="#"><b>丁辰灵</b></a>
-//		<p>【一张决策图来重新思考你的网站】你至少要考虑四个方面：目标&客户；内容&结构；设计；技术。为什么要建立自己的站点？内容和结构如何安排？网站设计能否脱颖而出？你的技术支持是否足够聪明和稳定？是时候重新思考你的网站！@图说PicSays “3W互联网深度精选”（微信studywww），最有价值深度干货。</p>
-//	</div>
-//</div>
 }
 
 function displayUserCard(card) {
 	var section = dojo.query('.l-side')[0];
 	section.innerHTML = wrapInTag(card.name, 'p') + wrapInTag(card.intro, 'p');
+}
+
+function showTags(tags) {
+	var str = '';
+	dojo.forEach(tags, function(tag){
+		str += tag.name;
+		str += ' ';
+	});
+	return str;
 }
 
 function wrapInTag(content, tagName) {
