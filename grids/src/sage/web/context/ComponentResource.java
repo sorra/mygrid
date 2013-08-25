e80b6b3b03685150537da9cc84581e30088c9b3c
dhu <dawnstar@163.com>
2013-08-25 20:49:25 +0800

    Components: No need to pass argument in pages

--- grids/src/sage/web/context/ComponentResource.java
+++ grids/src/sage/web/context/ComponentResource.java
@@ -1,11 +1,13 @@
 package sage.web.context;
 
 public class ComponentResource {
-    public static String includeCSS(String[] components) {
+    public static String includeCSS() {
+        String[] components = (String[]) WebContexts.getRequestBean("components");
         return ComponentResourceManager.instance().includeCSS(components);
     }
     
-    public static String includeJS(String[] components) {
+    public static String includeJS() {
+        String[] components = (String[]) WebContexts.getRequestBean("components");
         return ComponentResourceManager.instance().includeJS(components);
     }
 }
