56f7e3102b5b7e48c0dc2d1001e0ca20f9e366e2
dhu <dawnstar@163.com>
2013-11-15 21:56:35 +0800

    Verify the connection to search server, if not available, log an error and abandon later operations

--- grids/src/sage/domain/search/SearchBase.java
+++ grids/src/sage/domain/search/SearchBase.java
@@ -14,6 +14,8 @@ import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import sage.transfer.BlogData;
@@ -26,20 +28,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class SearchBase {
 	public static final String BD = "bd";
 	public static final String TC = "tc";
-	
 	private static final String INDEX = "sage";
+	
+	private static final Logger logger = LoggerFactory.getLogger(SearchBase.class);
+	
 	private static final Map<Class<?>, String> typeMap = new HashMap<>();
 	static {
 		typeMap.put(BlogData.class, BD);
 		typeMap.put(TweetCard.class, TC);
 	}
 	
-	private Client client;
+	private TransportClient client;
 	private ObjectMapper om = new ObjectMapper();
 	
 	SearchBase() {
 		client = new TransportClient()
 		  .addTransportAddresses(new InetSocketTransportAddress("localhost", 9300));
+		if (client.connectedNodes().isEmpty()) {
+		    logger.error("Cannot connect to search server!");
+		    client = null;
+		    return;
+		}
 		putMapping(getMappingSource("bd-mapping.json"), BD);
 		putMapping(getMappingSource("tc-mapping.json"), TC);
 	}
@@ -81,6 +90,7 @@ public class SearchBase {
 	 * @param object a transfer object
 	 */
 	public void index(long id, Object object) {
+	    if (client == null) return;
 		if (object == null) {
 			throw new IllegalArgumentException("object is null");
 		}
@@ -96,6 +106,7 @@ public class SearchBase {
 	}
 	
 	public void delete(Class<?> clazz, long id) {
+        if (client == null) return;
 		client.prepareDelete().setIndex(INDEX)
 			.setType(mapType(clazz)).setId(String.valueOf(id))
 			.execute();
